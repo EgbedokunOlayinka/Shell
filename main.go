33d1194fd9daf3aaf1114a2b0fa6e1061f7c139b
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2026-06-14 10:28:23 +0100

    start parsing commands

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -18,7 +21,43 @@ func main() {
 			os.Exit(1)
 		}
 
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
+		}
+		if strings.HasPrefix(command, "echo ") {
+			fmt.Println(command[5:])
+			continue
+		}
+		if strings.HasPrefix(command, "type ") {
+			arg := command[5:]
+			executeTypeCommand(arg)
+			continue
+		}
 		fmt.Println(command + ": command not found")
 	}
+}
+
+func executeTypeCommand(arg string) {
+	builtins := map[string]bool{
+		"echo": true,
+		"exit": true,
+		"type": true,
+	}
+	if builtins[arg] {
+		fmt.Println(arg + " is a shell builtin")
+		return
+	} 
+
+	path, err := exec.LookPath(arg)
+	if err != nil {
+		if errors.Is(err, exec.ErrNotFound) {
+			fmt.Println(arg + ": not found")
+		} else {
+			fmt.Println("error:", err)
+		}
+		return;
+	}
+	fmt.Println(arg + " is " + path)
 }
 
